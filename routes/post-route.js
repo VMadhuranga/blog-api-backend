065afc990fd54a477933185c261f76d69fc75100
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-13 11:52:18 +0530

    Add commentController to post router middleware chain

--- routes/post-route.js
+++ routes/post-route.js
@@ -1,8 +1,10 @@
 const express = require("express");
 
 const postController = require("../controllers/post-controller/index");
+const commentRouter = require("./comment-route");
 
 const router = express.Router();
+router.use(commentRouter);
 
 router.get("/", postController.getPosts);
 router.get("/:post_id", postController.getPost);
