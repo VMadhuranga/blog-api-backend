521adf6ef233fbfe55ce8ae3507166a507ba5b80
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-14 08:37:33 +0530

    Create router to handle POST comments request

--- routes/comment-route.js
+++ routes/comment-route.js
@@ -5,5 +5,6 @@ const commentController = require("../controllers/comment-controller/index");
 const router = express.Router();
 
 router.get("/:post_id/comments", commentController.getComments);
+router.post("/:post_id/comments", commentController.createComment);
 
 module.exports = router;
