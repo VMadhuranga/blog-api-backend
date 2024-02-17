d2ee932cd630aa27f91b2364cd782ad25c92d00e
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-17 14:17:32 +0530

    Add updatePost controller to post controller index

--- controllers/post-controller/index.js
+++ controllers/post-controller/index.js
@@ -1,9 +1,11 @@
 const getPosts = require("./get-posts");
 const getPost = require("./get-post");
 const createPost = require("./create-post");
+const updatePost = require("./update-post");
 
 module.exports = {
   getPosts,
   getPost,
   createPost,
+  updatePost,
 };
