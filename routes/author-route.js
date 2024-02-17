cd18aff929938195a6adda3bd0f7e14d208cafe9
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-17 09:14:23 +0530

    Add postRouter to authorRouter's middleware chain
    Additional:
    - Authenticate author/post route

--- routes/author-route.js
+++ routes/author-route.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const passport = require("passport");
 
 const authorController = require("../controllers/author-controller/index");
+const postRouter = require("./post-route");
 
 const router = express.Router();
 
 router.post("/login", authorController.loginAuthor);
+router.use(
+  "/posts",
+  passport.authenticate("jwt", { session: false }),
+  postRouter,
+);
 
 module.exports = router;
