130acab644ffa1037961ea7edbb2d76813b84d84
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-17 14:15:39 +0530

    Add condition to query posts data according authorized status

--- controllers/post-controller/get-posts.js
+++ controllers/post-controller/get-posts.js
@@ -3,9 +3,11 @@ const asyncHandler = require("express-async-handler");
 const PostModel = require("../../models/post-model");
 
 const getPosts = asyncHandler(async (req, res, next) => {
+  const isAuthorized = Boolean(req.headers.authorization);
+
   const posts = await PostModel.find(
-    { isPublished: true },
-    "title createdDate isPublished",
+    isAuthorized ? {} : { isPublished: true },
+    `title createdDate ${isAuthorized ? "isPublished" : ""}`,
   )
     .sort({ _id: 1 })
     .exec();
