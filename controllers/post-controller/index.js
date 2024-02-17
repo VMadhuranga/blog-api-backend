349c5244b794cb4198eac27ccef286d42e891dbb
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-17 11:15:31 +0530

    Add createPost controller to post controller index

--- controllers/post-controller/index.js
+++ controllers/post-controller/index.js
@@ -1,7 +1,9 @@
 const getPosts = require("./get-posts");
 const getPost = require("./get-post");
+const createPost = require("./create-post");
 
 module.exports = {
   getPosts,
   getPost,
+  createPost,
 };
