68356aad102de2d43989bcc305b402757797cfc6
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-18 09:19:30 +0530

    Add deleteComment controller to comment controller index

--- controllers/comment-controller/index.js
+++ controllers/comment-controller/index.js
@@ -1,7 +1,9 @@
 const getComments = require("./get-comments");
 const createComment = require("./create-comment");
+const deleteComment = require("./delete-comment");
 
 module.exports = {
   getComments,
   createComment,
+  deleteComment,
 };
