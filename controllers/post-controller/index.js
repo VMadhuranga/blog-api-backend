61c423e66d4eb223e0fb24f9e3078eafc137a213
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-18 08:37:36 +0530

    Add deletePost controller to post controller index

--- controllers/post-controller/index.js
+++ controllers/post-controller/index.js
@@ -2,10 +2,12 @@ const getPosts = require("./get-posts");
 const getPost = require("./get-post");
 const createPost = require("./create-post");
 const updatePost = require("./update-post");
+const deletePost = require("./delete-post");
 
 module.exports = {
   getPosts,
   getPost,
   createPost,
   updatePost,
+  deletePost,
 };
